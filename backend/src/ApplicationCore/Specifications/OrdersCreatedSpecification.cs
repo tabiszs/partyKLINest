047000d9b66c969ea7cdaecfc92fe28f145fb12d
Dawid Wysocki <dwysocki157@gmail.com>
2022-04-02 01:32:29 +0200

    Add order specifications

---
+++ backend/src/ApplicationCore/Specifications/OrdersCreatedSpecification.cs
@@ -0,0 +1,13 @@
+﻿using Ardalis.Specification;
+using PartyKlinest.ApplicationCore.Entities.Orders;
+
+namespace PartyKlinest.ApplicationCore.Specifications
+{
+    public class OrdersCreatedSpecification : Specification<Order>
+    {
+        public OrdersCreatedSpecification()
+        {
+            Query.Where(o => o.Status == OrderStatus.Active);
+        }
+    }
+}
