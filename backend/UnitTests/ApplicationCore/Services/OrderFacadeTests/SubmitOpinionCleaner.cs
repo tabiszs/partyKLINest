187d93b844ca5fe037eccb58e7b4332dedec59cf
Dawid Wysocki <dwysocki157@gmail.com>
2022-04-02 00:24:49 +0200

    Add order facade

---
+++ backend/UnitTests/ApplicationCore/Services/OrderFacadeTests/SubmitOpinionCleaner.cs
@@ -0,0 +1,57 @@
+﻿using Moq;
+using PartyKlinest.ApplicationCore.Entities.Orders;
+using PartyKlinest.ApplicationCore.Exceptions;
+using PartyKlinest.ApplicationCore.Interfaces;
+using PartyKlinest.ApplicationCore.Services;
+using System.Threading.Tasks;
+using UnitTests.Factories;
+using Xunit;
+
+namespace UnitTests.ApplicationCore.Services.OrderFacadeTests
+{
+    public class SubmitOpinionCleaner
+    {
+        private readonly Mock<IRepository<Order>> _mockOrderRepo = new();
+
+        [Fact]
+        public async Task ThrowsOrderNotFoundExceptionWhenThereIsNoOrderWithGivenId()
+        {
+            var orderBuilder = new OrderBuilder();
+            Order? returnedOrder = null;
+            var opinion = orderBuilder.TestOpinion;
+            long orderId = orderBuilder.TestOrderId;
+
+            _mockOrderRepo.Setup(x => x.GetByIdAsync(It.IsAny<long>(), default)).ReturnsAsync(returnedOrder);
+
+            var orderFacade = new OrderFacade(_mockOrderRepo.Object);
+
+            await Assert.ThrowsAsync<OrderNotFoundException>(() => orderFacade.SubmitOpinionCleanerAsync(orderId, opinion));
+        }
+
+        [Fact]
+        public async Task AddsCleanersOpinionToOrder()
+        {
+            var orderBuilder = new OrderBuilder();
+            orderBuilder.WithCleanerId();
+            var order = orderBuilder.Build();
+            var opinion = orderBuilder.TestOpinion;
+            var orderId = orderBuilder.TestOrderId;
+
+            _mockOrderRepo
+                .Setup(x => x.GetByIdAsync(It.IsAny<long>(), default))
+                .ReturnsAsync(order);
+
+            Assert.Null(order.CleanersOpinion);
+
+            var orderFacade = new OrderFacade(_mockOrderRepo.Object);
+            await orderFacade.SubmitOpinionCleanerAsync(orderId, opinion);
+
+            _mockOrderRepo.Verify(x =>
+                x.UpdateAsync(
+                    It.Is<Order>(o => o.CleanersOpinion != null && o.CleanersOpinion == opinion),
+                    default),
+                Times.Once
+                );
+        }
+    }
+}
---
+++ backend/src/ApplicationCore/Services/OrderFacade.cs
@@ -0,0 +1,42 @@
+﻿using PartyKlinest.ApplicationCore.Entities.Orders;
+using PartyKlinest.ApplicationCore.Entities.Orders.Opinions;
+using PartyKlinest.ApplicationCore.Exceptions;
+using PartyKlinest.ApplicationCore.Interfaces;
+using System.Threading.Tasks;
+
+namespace PartyKlinest.ApplicationCore.Services
+{
+    public class OrderFacade
+    {
+        public OrderFacade(IRepository<Order> orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        private readonly IRepository<Order> _orderRepository;
+
+        public async Task<Order> GetOrderAsync(long orderId)
+        {
+            var order = await _orderRepository.GetByIdAsync(orderId);
+            if (order is null)
+            {
+                throw new OrderNotFoundException(orderId);
+            }
+            return order;
+        }
+
+        public async Task SubmitOpinionCleanerAsync(long orderId, Opinion cleanerOpinion)
+        {
+            var order = await GetOrderAsync(orderId);
+            order.SetCleanersOpinion(cleanerOpinion);
+            await _orderRepository.UpdateAsync(order);
+        }
+
+        public async Task SubmitOpinionClientAsync(long orderId, Opinion cleanerOpinion)
+        {
+            var order = await GetOrderAsync(orderId);
+            order.SetClientsOpinion(cleanerOpinion);
+            await _orderRepository.UpdateAsync(order);
+        }
+    }
+}
