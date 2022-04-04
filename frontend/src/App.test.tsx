ac64655beadf4856cda210abd61e5af9787bcc19
zh-qs <szymontur3@o2.pl>
2022-04-04 02:18:42 +0200

    mock msal function in tests

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -3,6 +3,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  jest.mock('./Authentication/MsalService',() => ({ RetrieveToken: jest.fn(() => undefined) }));
   render(<App />);
   const linkElement = screen.getByText(/PartyKLINer/i);
   expect(linkElement).toBeInTheDocument();
