2a7db160c5fe167723b0cef458d57b35f75710dc
Szymon Zyguła <szymon@zygula.eu>
2022-04-09 15:13:32 +0200

    Add order deletion by organizer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
-import { useEffect, useState } from 'react';
-import { B2CLogin, B2CLogout, RetrieveToken } from './Authentication/MsalService';
+import {useEffect, useState} from 'react';
+import {B2CLogin, B2CLogout, RetrieveToken} from './Authentication/MsalService';
 import Layout from './Layout';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Dashboard from './ContentScreens/Dashboard';
 import Settings from './ContentScreens/Settings';
 import PostAnnouncement from './ContentScreens/PostAnnouncement';
+import OrderDeletion from './ContentScreens/Admin/OrderDeletion';
+import OrderManagement from './ContentScreens/ClientOrderManagement';
 
 const App = () => {
 
@@ -34,10 +36,12 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout isLogged={isLogged} login={login} logout={logout}/>}>
-          <Route index element={<Dashboard/>}/>
-          <Route path="/settings" element={<Settings logout={() => setIsLogged(false)}/>}/>
-          <Route path="/postAnnouncement" element={<PostAnnouncement/>}/>
+        <Route path="/" element={<Layout isLogged={isLogged} login={login} logout={logout} />}>
+          <Route index element={<Dashboard />} />
+          <Route path="/settings" element={<Settings logout={() => setIsLogged(false)} />} />
+          <Route path="/postAnnouncement" element={<PostAnnouncement />} />
+          <Route path="/orderDeletion" element={<OrderDeletion />} />
+          <Route path="/orderManagement" element={<OrderManagement />} />
         </Route>
       </Routes>
     </BrowserRouter>
