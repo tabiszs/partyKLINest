96d04da662d68cfd6ecd0ed9952fa4f544f9671e
TortillaZHawaii <dwysocki157@gmail.com>
2022-04-23 18:27:34 +0200

    Add DateTimeOffsetExtension test

---
+++ backend/tests/UnitTests/ApplicationCore/Extensions/DateTimeOffsetExtensionTest/ToTimeOnly.cs
@@ -0,0 +1,19 @@
+using System;
+using PartyKlinest.ApplicationCore.Extensions;
+using Xunit;
+
+namespace UnitTests.ApplicationCore.Extensions.DateTimeOffsetExtensionTest;
+
+public class ToTimeOnly
+{
+    [Fact]
+    public void GetsCorrectTime()
+    {
+        var dateTimeOffset = new DateTimeOffset(2022, 2, 22, 16, 22, 15, TimeSpan.Zero);
+        var expected = new TimeOnly(16, 22, 15);
+
+        var actual = dateTimeOffset.ToTimeOnly();
+        
+        Assert.Equal(expected, actual);
+    }
+}
