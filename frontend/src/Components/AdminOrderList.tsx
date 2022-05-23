36b5e2518d4ee972d6b3eaf2dee4f5facc6c3689
Szymon Zyguła <szymon@zygula.eu>
2022-05-23 10:25:37 +0200

    Fix assign button

--- frontend/src/Components/AdminOrderList.tsx
+++ frontend/src/Components/AdminOrderList.tsx
@@ -52,8 +52,9 @@ const OrderCard = (props: OrderCardProps) => {
           <div className='card-column'>
             <CardButton label={props.deleteButtonLabel} onClick={props.onDeleteButtonClick} color="error" />
             {
-              props.order.cleanerId !== undefined || props.order.cleanerId !== null || props.order.status !== OrderStatus.Active ? null
-              : <CardButton label={props.assignButtonLabel} onClick={props.onAssignButtonClick} color="primary" />
+              (props.order.cleanerId === undefined || props.order.cleanerId === null) && props.order.status === OrderStatus.Active
+                ? <CardButton label={props.assignButtonLabel} onClick={props.onAssignButtonClick} color="primary" />
+                : null
             }
           </div>
         </div>
